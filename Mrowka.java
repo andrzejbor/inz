8a6b33bb9abd05ec0b7fffe6015e3353e4620b18
andrzejboran <borowiak.and@gmail.com>
2017-03-29 22:14:43 +0200

    Dodanie klasy Trasa i losowanie pierwszego portu

---
+++ Mrowka.java
@@ -0,0 +1,41 @@
+package sample;
+
+import java.util.Random;
+
+/**
+ * Created by Andrzej on 29.03.2017.
+ */
+public class Mrowka {
+
+    private int numer;
+
+    private int[] trasa;
+
+    public Mrowka(int numer) {
+        this.numer = numer;
+    }
+
+    public int getNumer() {
+        return numer;
+    }
+
+    public void setNumer(int numer) {
+        this.numer = numer;
+    }
+
+    public int[] getTrasa() {
+        return trasa;
+    }
+
+    public void setTrasa(int[] trasa) {
+        this.trasa = trasa;
+    }
+
+    public int losujPierwszyPort(){
+        Random random = new Random();
+        int wylosowanyPort;
+        wylosowanyPort = random.nextInt(3) + 1;
+        return wylosowanyPort;
+    }
+
+}
